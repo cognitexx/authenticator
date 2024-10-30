c3bfde45ce9edd65d0e2610ba7d629d80c2a3938
Sebastian Villena <97059974+ruisebas@users.noreply.github.com>
2024-10-30 14:21:36 -0400

    Update Sources/Authenticator/Views/Internal/ConfirmSignInWithCodeView.swift

--- Sources/Authenticator/Views/Internal/ConfirmSignInWithCodeView.swift
+++ Sources/Authenticator/Views/Internal/ConfirmSignInWithCodeView.swift
@@ -72,7 +72,7 @@ struct ConfirmSignInWithCodeView<Header: View,
     }
 
     private var submitButtonTitle: String {
-        switch getCurrentMFAType {
+        switch currentMFAType {
         case .sms, .none, .email:
             return "authenticator.confirmSignInWithCode.button.submit".localized()
         case .totp:
